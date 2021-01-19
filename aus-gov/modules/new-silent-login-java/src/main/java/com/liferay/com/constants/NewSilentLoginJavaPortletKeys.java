12001cfc01a0c62a9ce61bb6cda2192b9c98ac75
Roselaine Marques <roselaine.marques@liferay.com>
2021-01-19 11:59:39 +0100

    eliminated some unnecessary dependencies and constants

--- aus-gov/modules/new-silent-login-java/src/main/java/com/liferay/com/constants/NewSilentLoginJavaPortletKeys.java
+++ aus-gov/modules/new-silent-login-java/src/main/java/com/liferay/com/constants/NewSilentLoginJavaPortletKeys.java
@@ -7,11 +7,4 @@ public class NewSilentLoginJavaPortletKeys {
 
 	public static final String NEWSILENTLOGINJAVA =
 		"com_liferay_com_NewSilentLoginJavaPortlet";
-
-	public static final String SERVLET_PATH_CALLBACK = "oidc/callback/SAGOV-Idp/medfile-react-client-app";
-
-	public static final String SERVLET_PATH_PROVIDER = "oidc/providerinfo/";
-
-	public static final String ID_PROVIDER = "SAGOV-Idp";
-
 }
