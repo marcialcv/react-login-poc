f026ac21d76bba2078b7382a36d800b9ff25da08
Roselaine Marques <roselaine.marques@liferay.com>
2021-03-02 10:42:57 +0100

    template for hybrid angular portlet with version 10

---
+++ aus-gov/modules/silent-login-angular-app/src/main/java/silent/login/angular/app/portlet/SilentLoginAngularAppPortlet.java
@@ -0,0 +1,51 @@
+package silent.login.angular.app.portlet;
+
+import silent.login.angular.app.constants.SilentLoginAngularAppPortletKeys;
+
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author roselainedefaria
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + SilentLoginAngularAppPortletKeys.SilentLoginAngularApp,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class SilentLoginAngularAppPortlet extends MVCPortlet {
+
+	@Override
+	public void doView(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		renderRequest.setAttribute(
+			"mainRequire",
+			_npmResolver.resolveModuleName("silent-login-angular-app") + " as main");
+
+		super.doView(renderRequest, renderResponse);
+	}
+
+	@Reference
+	private NPMResolver _npmResolver;
+
+}
